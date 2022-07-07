aa33ac1e0e24f7871dd8e14028dec61527d73d61
LoliTeamCyber <101130397+LoliTeamCyber@users.noreply.github.com>
2022-07-07 20:14:26 +0700

    Add files via upload
    FREE SMS

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+echo $banner = "
+
+\x1b[36m╔══╗╔═╦═╗╔══╗
+║══╣║║║║║║══╣
+╠══║║║║║║╠══║
+╚══╝╚╩═╩╝╚══╝\x1b[32mFREE\x1b[31m ❷.Ѳ
+
+\x1b[32m● YouTube :\x1b[93m Android Security
+\x1b[32m● Mail : \x1b[93mandroidsec@my.id
+\x1b[32m● WEB : \x1b[93mandroidsec.my.id
+\n";
